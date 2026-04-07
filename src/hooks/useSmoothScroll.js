b7929863c7fc0411cd59379f896d799bbd719ba2
Aaditya-2407 <kharataaditya2407@gmail.com>
2026-04-08 00:50:03 +0530

    removed triggers from mobile

--- src/hooks/useSmoothScroll.js
+++ src/hooks/useSmoothScroll.js
@@ -1,9 +1,6 @@
 import { useEffect } from 'react'
 import Lenis from 'lenis'
 import gsap from 'gsap'
-import { ScrollTrigger } from 'gsap/ScrollTrigger'
-
-gsap.registerPlugin(ScrollTrigger)
 
 export default function useSmoothScroll() {
   useEffect(() => {
@@ -19,16 +16,12 @@ export default function useSmoothScroll() {
       wheelMultiplier: 0.9,
     })
 
-    lenis.on('scroll', ScrollTrigger.update)
-
     const update = (time) => {
       lenis.raf(time * 1000)
     }
 
     gsap.ticker.add(update)
     gsap.ticker.lagSmoothing(0)
-
-    ScrollTrigger.refresh()
 
     return () => {
       gsap.ticker.remove(update)
